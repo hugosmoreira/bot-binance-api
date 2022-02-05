d2f1daceb70157525fb8f18c46c60708d5eb40c6
hugosmoreira <hugo.n4@gmail.com>
2022-02-04 17:39:07 -0800

    primeiro dia completo

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ async function getData() {
       const axios = require('axios');
       
     
-      const response = await axios.get('https://api.binance.us/api/v3/klines?symbol=BTCUSDT&interval=15m&limit=2')
-      console.log(response.data);
+      const response = await axios.get('https://api.binance.us/api/v3/klines?symbol=BTCUSDT&interval=1m')
+      const candle = response.data[499];
+      const price = parseFloat(candle[4]);
+      if(price>= 36291.5) {
+            console.log('Price is high Vender!!');
+      } else if(price <= 35764) {
+            console.log('Price is low Compre!!');
+      } else {
+            console.log('Price is normal HOLD!');
+      }
+      
 
 };
+
+setInterval(getData, 1000);
 
     
 getData();
