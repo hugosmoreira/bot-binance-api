167b8290a8c858001d001fc37e31e2864ec9ba27
hugosmoreira <hugo.n4@gmail.com>
2022-02-04 18:40:11 -0800

    update telegram messages with the bot and env file

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+require('dotenv').config()
+
+// API telegram token ID 
 
 
 async function getData() {
+      const { Telegraf } = require('telegraf')
+      const bot = new Telegraf(process.env.BOT_TOKEN)
       const axios = require('axios');
+      
+
       
     
       const response = await axios.get('https://api.binance.us/api/v3/klines?symbol=BTCUSDT&interval=1m')
@@ -9,10 +16,14 @@ async function getData() {
       const price = parseFloat(candle[4]);
       if(price>= 36291.5) {
             console.log('Price is high Vender!!');
+            bot.telegram.sendMessage(process.env.CHAT_ID, 'Price is high Vender!!');
+
       } else if(price <= 35764) {
             console.log('Price is low Compre!!');
+            bot.telegram.sendMessage(process.env.CHAT_ID, 'Price is low Compre!!');
       } else {
             console.log('Price is normal HOLD!');
+            bot.telegram.sendMessage(process.env.CHAT_ID, 'Price is normal HOLD!');
       }
       
 
